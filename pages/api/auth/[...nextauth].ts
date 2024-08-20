fe8a6267d0832d4d390c1e379aac557e0bb8bf64
Adam <durigonadam@gmail.com>
2024-08-20 23:27:00 +0200

    final commit

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,7 +3,7 @@ import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcrypt"
 
 import prisma from "@/app/libs/prismadb"
 
